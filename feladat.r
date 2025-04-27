0e0128903f5ab4ff18731cb325dc6e4c6cb92d86
kborbala <kecza.borbala@gmail.com>
2025-04-27 20:05:25 +0200

    feladat.r módosítása
    Új változó létrehozása a kódban.

--- feladat.r
+++ feladat.r
@@ -1,6 +1,7 @@
 print('Hello world')
 
 szam = 1
+szam2 = 7 #új változó létrehozása
 
-szamok = szam + szam
-print(szamok)
+szamok = szam + szam2 #a régi és új szám összeadásaa
+print(szamok)  #eredmény kiiratása
